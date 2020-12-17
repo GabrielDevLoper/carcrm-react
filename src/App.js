f388692d461126167076429121ec69a4d3316ec9
Gabriel <gabriel.devloper@gmail.com>
2020-12-16 21:51:00 -0300

    apresentação dos componentes da biblioteca material ui e padronizando algumas propriedades dos componentes.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Button, TextField, Select } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ff4400",
+    },
+  },
+
+  props: {
+    MuiTextField: {
+      variant: "outlined",
+      fullWidth: true,
+    },
+
+    MuiSelect: {
+      variant: "outlined",
+      fullWidth: true,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <h1>Olá mundo</h1>
-    </>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+
+      <TextField />
+    </ThemeProvider>
   );
 }
 
